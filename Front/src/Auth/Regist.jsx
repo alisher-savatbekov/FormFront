71af5b28216a77292e7d8b5bd613dfabca912af8
@alihsersavatbekov <alihsersavatbekov@gmail.com>
2024-12-18 12:10:49 +0500

    Registarate Component Processing

--- Front/src/Auth/Regist.jsx
+++ Front/src/Auth/Regist.jsx
@@ -1,70 +1,105 @@
-import { Button,FloatingLabel,Form } from 'react-bootstrap';
+import  Button  from 'react-bootstrap/Button';
 import * as formik from 'formik';
 import * as yup from 'yup';
-
-
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 function Regist(){
     const {Formik}=formik;
-    const schema=yup.object().shape({
-
-            username:yup.string().min(2).required(),
-            
-            email:yup.string().required(),
-            
-            password:yup.string().min(6,"at least minmum 6 symbol").required(),
-            
-            confirmPassword:yup.string.oneOf([yup.ref("password",null)]).required(),
-
-            terms:yup.bool().required.onOf([true],'Term must be accepted')
-    });
-
-    
-
-
+    const schema = yup.object().shape({
+        username: yup.string().min(2, "At least 2 characters").required("Username is required"),
+        email: yup.string().email("Invalid email").required("Email is required"),
+        password: yup.string().min(6, "At least 6 characters").required("Password is required"),
+        confirmPassword: yup
+          .string()
+          .oneOf([yup.ref("password"), null], "Passwords must match")
+          .required("Confirm password is required"),
+      });
     return(
 
         <div>
             <Formik
                     validationSchema={schema}
-                    onSubmit={console.log}
+                    onSubmit={(values)=>{
+                        console.log(values)
+                    }
+                        
+                    }
                 initialValues={
                  {
                     username:"",
                     password:"",
                     confirmPassword:"",
                     email:"",
-                    terms:false
-
                  }
                 }
-
             >
                 {
-                    ()=>
+                    ({handleSubmit,handleChange,errors,values,touched})=>
                     (
-                        <Form>
-                            <FloatingLabel controlId='floatingUsername' label="UserName" className='mb-3'>
-                                <Form.Control/>
+                        <Form onSubmit={handleSubmit}
+                        validateOnChange={true} // Отключает валидацию при каждом изменении
+                        validateOnBlur={false}    // Валидация произойдет при потере фокуса
+                        noValidate>
+                            <FloatingLabel controlId='floatingUsername' label="UserName" className='mb-3' >
+                                <Form.Control type="text" name="username" 
+                                value={values.username}
+                                placeholder='UerName'
+                                onChange={handleChange} 
+                                isInvalid={
+                                    !!errors.username && touched.username
+                                }
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {errors.username}
+                                </Form.Control.Feedback>
                             </FloatingLabel>
 
                             <FloatingLabel controlId='floatingEmil' label="Email Adress" className='mb-3'>
-                                <Form.Control/>
+                                <Form.Control
+                                type='email'
+                                 placeholder='name@example.com'
+                                 onChange={handleChange}
+                                 name="email"
+                                 value={values.email}
+                                 isInvalid={!!errors.email && touched.username}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {errors.email}
+                                </Form.Control.Feedback>
                             </FloatingLabel>
                             
                             <FloatingLabel controlId='floatingPassword' label="Password" className='mb-3'>
-                                <Form.Control/>
+                                <Form.Control
+                                placeholder='Paasword'
+                                onChange={handleChange}
+                                type='password'
+                                name="password"
+                                value={values.password}
+                                isInvalid={!!errors.password && touched.password}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {errors.password}
+                                </Form.Control.Feedback>
                             </FloatingLabel>
                             
                             <FloatingLabel contolId="floatingConfirmPassword" label="ConfirmPassword" className='mb-3'>
-                                <Form.Control/>
+                                <Form.Control 
+                                placeholder='ConfirmPassword'
+                                onChange={handleChange}
+                                name="confirmPassword"
+                                type='password'
+                                value={values.confirmPassword}
+                                isInvalid={!!errors.confirmPassword && touched.confirmPassword}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {errors.confirmPassword}
+                                </Form.Control.Feedback>
                             </FloatingLabel>
-                            <Button type="submit">Registrate</Button>
+                            <Button type="submit">Submit</Button>
                         </Form>
                     )
-                }
-                
-
+                }       
             </Formik>
         </div>
     )
