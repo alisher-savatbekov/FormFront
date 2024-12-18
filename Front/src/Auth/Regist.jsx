94684280d88f65b3db44933ad2ffdb7c2270c530
@alihsersavatbekov <alihsersavatbekov@gmail.com>
2024-12-18 10:20:28 +0500

    Form validation Processing

--- Front/src/Auth/Regist.jsx
+++ Front/src/Auth/Regist.jsx
@@ -1,8 +1,72 @@
+import { Button,FloatingLabel,Form } from 'react-bootstrap';
+import * as formik from 'formik';
+import * as yup from 'yup';
+
+
 
 function Regist(){
+    const {Formik}=formik;
+    const schema=yup.object().shape({
+
+            username:yup.string().min(2).required(),
+            
+            email:yup.string().required(),
+            
+            password:yup.string().min(6,"at least minmum 6 symbol").required(),
+            
+            confirmPassword:yup.string.oneOf([yup.ref("password",null)]).required(),
+
+            terms:yup.bool().required.onOf([true],'Term must be accepted')
+    });
+
+    
+
 
     return(
-        <div></div>
+
+        <div>
+            <Formik
+                    validationSchema={schema}
+                    onSubmit={console.log}
+                initialValues={
+                 {
+                    username:"",
+                    password:"",
+                    confirmPassword:"",
+                    email:"",
+                    terms:false
+
+                 }
+                }
+
+            >
+                {
+                    ()=>
+                    (
+                        <Form>
+                            <FloatingLabel controlId='floatingUsername' label="UserName" className='mb-3'>
+                                <Form.Control/>
+                            </FloatingLabel>
+
+                            <FloatingLabel controlId='floatingEmil' label="Email Adress" className='mb-3'>
+                                <Form.Control/>
+                            </FloatingLabel>
+                            
+                            <FloatingLabel controlId='floatingPassword' label="Password" className='mb-3'>
+                                <Form.Control/>
+                            </FloatingLabel>
+                            
+                            <FloatingLabel contolId="floatingConfirmPassword" label="ConfirmPassword" className='mb-3'>
+                                <Form.Control/>
+                            </FloatingLabel>
+                            <Button type="submit">Registrate</Button>
+                        </Form>
+                    )
+                }
+                
+
+            </Formik>
+        </div>
     )
 
 }
